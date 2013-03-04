f963e2eab96b82509a6300f070b9956ce8bfdf9a
Kyle Rimkus <krimkus@concentricsky.com>
2013-03-04 15:16:09 -0800

    Merging redesign2013 from https://github.com/barto457/django-client-admin; 45073d2 to 7fd651c

--- client_admin/widgets.py
+++ client_admin/widgets.py
@@ -4,25 +4,25 @@ from django.conf import settings
 
 
 def thumbnail(image_path):
-    return u'<img src="%s" class="imageupload-thumbnail">' % image_path
+   return u'<img src="%s" class="imageupload-thumbnail">' % image_path
 
 
 class ThumbnailImageWidget(AdminFileWidget):
-    template_with_initial = '<p class="file-upload">%(input)s<br />%(clear_template)s</p>'
-    clear_checkbox_label = "Delete"
+    template_with_initial = '<p class="file-upload">%(input)s%(clear_template)s</p>'
+    clear_checkbox_label = "<span>Undo</span> Delete Image"
 
     def render(self, name, value, attrs=None):
         output = []
         if value:
             file_path = '%s%s' % (getattr(settings, 'MEDIA_URL', '/media/'), value)
             try:
-                output.append('<a target="_blank" href="%s">%s</a><br />' %
-                        (file_path, thumbnail(file_path)))
+                output.append('<a target="_blank" href="%s" class="imageupload-thumbcontainer">%s</a>' %
+                       (file_path, thumbnail(file_path)))
             except IOError:
                 output.append('%s <a target="_blank" href="%s">%s</a> <br />%s ' %
-                        ('Currently:', file_path, value, 'Change:'))
+                       ('Currently:', file_path, value, 'Change:'))
         else:
-            output.append(thumbnail('%simg/missing_image.png' % getattr(settings, 'STATIC_URL', '/static/')))
+            output.append(thumbnail('%sclient_admin/images/missing_image.png' % getattr(settings, 'STATIC_URL', '/static/')))
 
-        output.append('<div class="imageupload-widget"><p class="file-upload">%s</p></div>' % super(ThumbnailImageWidget, self).render(name, value, attrs))
+        output.append('<div class="imageupload-widget"><a href="#" class="file_upload_button">Change Image</a><p class="file-upload">%s</p></div>' % super(ThumbnailImageWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
