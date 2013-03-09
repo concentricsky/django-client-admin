1f3b4efa423483036b69b67f77966ebfa4c0bb0a
Kyle Rimkus <krimkus@concentricsky.com>
2013-03-08 16:25:39 -0800

    Renamed BaseGenericModelAdmin to GenericModelAdminMixin

--- client_admin/admin.py
+++ client_admin/admin.py
@@ -382,31 +382,35 @@ class ReverseInlinesModelAdmin(admin.ModelAdmin):
         return inline_instances
 
 
-# ## BaseGenericModelAdmin
-class BaseGenericModelAdmin(object):
+# ## GenericModelAdminMixin
+class GenericModelAdminMixin(object):
 
     class Media:
         js = ()
 
     content_type_lookups = {}
-    
+
     def __init__(self, model, admin_site):
         media = list(getattr(self.Media, 'js', ()))
         media.append(JS_PATH + 'genericadmin.js')
         self.Media.js = tuple(media)
-        super(BaseGenericModelAdmin, self).__init__(model, admin_site)
+        super(GenericModelAdminMixin, self).__init__(model, admin_site)
 
     def get_urls(self):
-        base_urls = super(BaseGenericModelAdmin, self).get_urls()
+        base_urls = super(GenericModelAdminMixin, self).get_urls()
         opts = self.get_generic_relation_options()
-        custom_urls = patterns('',
+        custom_urls = patterns(
+            '',
             url(r'^obj/$', self.admin_site.admin_view(generic_lookup), name='admin_genericadmin_obj_lookup'),
-            url(r'^get-generic-rel-list/$', self.admin_site.admin_view(get_generic_rel_list), kwargs=opts, 
+            url(
+                r'^get-generic-rel-list/$',
+                self.admin_site.admin_view(get_generic_rel_list),
+                kwargs=opts,
                 name='admin_genericadmin_rel_list'),
         )
         return custom_urls + base_urls
 
-    # Return a dictionary of keywords that are fed to the get_generic_rel_list view 
+    # Return a dictionary of keywords that are fed to the get_generic_rel_list view
     def get_generic_relation_options(self):
         return {'url_params': self.content_type_lookups,
                 'blacklist': self.get_blacklisted_relations(),
@@ -425,38 +429,6 @@ class BaseGenericModelAdmin(object):
             return ()
 
 
-class GenericAdminModelAdmin(BaseGenericModelAdmin, admin.ModelAdmin):
-    # Model admin for generic relations.
-    pass
-
-
-class GenericTabularInline(BaseGenericModelAdmin, generic.GenericTabularInline):
-    # Model admin for generic tabular inlines.
-    pass
-
-
-class GenericStackedInline(BaseGenericModelAdmin, generic.GenericStackedInline):
-    # Model admin for generic stacked inlines.
-    pass
-
-
-class ReverseInlinesModelAdmin(admin.ModelAdmin):
-    
-    def get_inline_instances(self, request):
-        inline_instances = super(ReverseInlinesModelAdmin, self).get_inline_instances(request)
-        for inline_class in self.inverse_inlines:
-            inline = inline_class(self.model, self.admin_site)
-            if request:
-                if not (inline.has_add_permission(request) or
-                        inline.has_change_permission(request) or
-                        inline.has_delete_permission(request)):
-                    continue
-                if not inline.has_add_permission(request):
-                    inline.max_num = 0
-            inline_instances.append(inline)
-        return inline_instances
-
-
 class URLFieldMixin(object):
     formfield_overrides = {
         URLField: {'widget': AdminURLFieldWidget},
@@ -470,5 +442,20 @@ class ImageWidgetMixin(object):
     }
 
 
-class ClientModelAdmin(ImageWidgetMixin, admin.ModelAdmin):
+class GenericAdminModelAdmin(GenericModelAdminMixin, admin.ModelAdmin):
+    # Model admin for generic relations.
+    pass
+
+
+class GenericTabularInline(GenericModelAdminMixin, generic.GenericTabularInline):
+    # Model admin for generic tabular inlines.
+    pass
+
+
+class GenericStackedInline(GenericModelAdminMixin, generic.GenericStackedInline):
+    # Model admin for generic stacked inlines.
+    pass
+
+
+class ClientModelAdmin(ImageWidgetMixin, GenericModelAdminMixin, admin.ModelAdmin):
     pass
