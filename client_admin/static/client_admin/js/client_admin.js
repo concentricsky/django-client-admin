31ca2f5e7ca2e4304969afef2d075603c24ca244
Kyle Rimkus <krimkus@concentricsky.com>
2013-03-11 15:18:02 -0700

    Removed relatedobjectlookups override in client_admin.js and instead overriding entire RelatedObjectLookups.js file

--- client_admin/static/client_admin/js/client_admin.js
+++ client_admin/static/client_admin/js/client_admin.js
@@ -11,17 +11,3 @@
         window.location.href = this.value;
     });
 })(django.jQuery);
-
-function showRelatedObjectLookupPopup(triggeringLink) {
-    var name = triggeringLink.id.replace(/^lookup_/, '');
-    name = id_to_windowname(name);
-    var href;
-    if (triggeringLink.href.search(/\?/) >= 0) {
-        href = triggeringLink.href + '&pop=1';
-    } else {
-        href = triggeringLink.href + '?pop=1';
-    }
-    var win = window.open(href, name, 'height=600,width=1000,resizable=yes,scrollbars=yes');
-    win.focus();
-    return false;
-}
