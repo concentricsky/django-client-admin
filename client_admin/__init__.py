732175b0bf85f32aade822e2388d0c8c684d01c7
Kyle Rimkus <krimkus@concentricsky.com>
2013-10-10 11:25:02 -0700

    Moved fix from a6d80f75 to just affect field help_text, not the entire field container, since that was breaking any grouped fields

--- client_admin/__init__.py
+++ client_admin/__init__.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 """
-django-client-admin is a collection of extensions/tools for the default django 
+django-client-admin is a collection of extensions/tools for the default django
 administration interface, including a dashboard, a customizable menu bar, and
 
 """
-VERSION = '1.0.0'
+VERSION = '1.0.1'
